d7152e8d4f14b9cf79174177e4cfc75986c5d77e
Sarah ali <sara.aliebra@gmail.com>
2024-11-04 14:26:11 +0300

    Initial Commit

---
+++ Appremind/AppremindApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppremindApp.swift
+//  Appremind
+//
+//  Created by Sarah ali  on 02/05/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct AppremindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appremind/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Appremind
+//
+//  Created by Sarah ali  on 02/05/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
